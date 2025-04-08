93214d4c07e3a717cfc7162f955787175f42e1d5
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2025-04-08 08:29:48 -0300

    Create temperatura.py
    A main foi criada com o objetivo de analisar a entrada de temperatura e converter graus Celsius para Fahrenheit e vice-versa.

---
+++ programacao-analise/temperatura.py
@@ -0,0 +1,16 @@
+# -*- coding:UTF-8 -*-
+
+print("Olá usuário! Digite uma temperatura em graus Celsius (°C) e eu transformarei para graus Fahrenheit (ºF) e vice-versa")
+
+
+medida = str(input("Digite C para Celsius e F para Fahreinheit: "))
+
+
+
+    conv = 0
+    if medida == "C":
+        conv = temperatura * 9/5 + 32
+        print ("O resultado da conversão é: ", conv)
+
+conversao(temperatura, medida)
+
