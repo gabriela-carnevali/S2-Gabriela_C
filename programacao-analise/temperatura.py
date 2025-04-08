ba931b22bd419995a1168d003480b52bb08c817c
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2025-04-08 08:33:33 -0300

    Update temperatura.py
    A nova branch foi inserida com novas linhas de código para aprimorar a eficiência do programa.

--- programacao-analise/temperatura.py
+++ programacao-analise/temperatura.py
@@ -2,15 +2,16 @@
 
 print("Olá usuário! Digite uma temperatura em graus Celsius (°C) e eu transformarei para graus Fahrenheit (ºF) e vice-versa")
 
+temp = float(input("Digite a temperatura em graus: "))
+medida = int(input("Digite C para Celsius e F para Fahreinheit: "))
 
-medida = str(input("Digite C para Celsius e F para Fahreinheit: "))
 
+while conversao (temperatura, medida):
 
-
-    conv = 0
     if medida == "C":
         conv = temperatura * 9/5 + 32
         print ("O resultado da conversão é: ", conv)
-
+    elif medida == "F":
+        conv = (temperatura - 32) * 5/9
+        print ("O resultado da conversão é: ", conversao)
 conversao(temperatura, medida)
-
